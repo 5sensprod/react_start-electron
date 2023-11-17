bf09e35b236d0ff4942b4f2dfee853f7dbd971b0
chris <5sensprod@gmail.com>
2023-11-17 09:58:19 +0100

    refacto

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import productSchema from '../schemas/productSchema'
 
-const FormField = ({ fieldKey, fieldSchema, value, onChange }) => {
+const FormField = ({
+  fieldKey,
+  fieldSchema: { type, required },
+  value,
+  onChange,
+}) => {
   const { t } = useTranslation()
-  const inputType = fieldSchema.type === String ? 'text' : 'number'
+  const inputType = type === String ? 'text' : 'number'
   const label = t(`productForm.labels.${fieldKey}`)
 
   return (
@@ -14,7 +19,7 @@ const FormField = ({ fieldKey, fieldSchema, value, onChange }) => {
         type={inputType}
         value={value}
         onChange={(e) => onChange(fieldKey, e.target.value)}
-        required={fieldSchema.required}
+        required={required}
       />
     </label>
   )
@@ -23,20 +28,17 @@ const FormField = ({ fieldKey, fieldSchema, value, onChange }) => {
 const ProductForm = ({ productData, onProductDataChange }) => {
   return (
     <div>
-      {Object.keys(productSchema).map((key) => {
-        const value = productData[key] !== undefined ? productData[key] : ''
-        return (
-          <FormField
-            key={key}
-            fieldKey={key}
-            fieldSchema={productSchema[key]}
-            value={value}
-            onChange={(fieldKey, fieldValue) =>
-              onProductDataChange({ ...productData, [fieldKey]: fieldValue })
-            }
-          />
-        )
-      })}
+      {Object.keys(productSchema).map((key) => (
+        <FormField
+          key={key}
+          fieldKey={key}
+          fieldSchema={productSchema[key]}
+          value={productData[key] || ''}
+          onChange={(fieldKey, fieldValue) =>
+            onProductDataChange({ ...productData, [fieldKey]: fieldValue })
+          }
+        />
+      ))}
     </div>
   )
 }
