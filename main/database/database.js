25af5a11f37a37e60d6cb6a43ce8274f44eb1a93
chris <5sensprod@gmail.com>
2023-11-20 11:43:23 +0100

    db build et dev

--- main/database/database.js
+++ main/database/database.js
@@ -1,12 +1,8 @@
 const Datastore = require('nedb')
 const path = require('path')
-const { app } = require('electron')
 
-// Calculer le chemin de base pour la base de données en fonction de l'environnement
-const isPackaged = app.isPackaged // ou utilisez 'process.env.NODE_ENV === "production"' si vous définissez NODE_ENV
-const baseDatabasePath = isPackaged
-  ? path.join(process.resourcesPath, 'data') // Chemin en production
-  : path.join(__dirname, '../../data') // Chemin en développement
+// Chemin constant pour le dossier de données de l'application
+const baseDatabasePath = path.join(__dirname, '../../data') // Chemin relatif au répertoire de la base de données
 
 let db = {}
 
@@ -26,3 +22,32 @@ db.categories = new Datastore({
 })
 
 module.exports = db
+
+//pour le buid
+// const Datastore = require('nedb')
+// const path = require('path')
+// const { app } = require('electron')
+
+// const isPackaged = app.isPackaged
+// const baseDatabasePath = isPackaged
+//   ? path.join(process.resourcesPath, 'data')
+//   : path.join(__dirname, '../../data')
+
+// let db = {}
+
+// db.users = new Datastore({
+//   filename: path.join(baseDatabasePath, 'users.db'),
+//   autoload: true,
+// })
+
+// db.products = new Datastore({
+//   filename: path.join(baseDatabasePath, 'products.db'),
+//   autoload: true,
+// })
+
+// db.categories = new Datastore({
+//   filename: path.join(baseDatabasePath, 'categories.db'),
+//   autoload: true,
+// })
+
+// module.exports = db
