440ae9f8d287d67fbd04ba0eeafc0f391254ad18
chris <5sensprod@gmail.com>
2023-11-20 15:18:30 +0100

    ajout catalogue au build

--- src/components/product/ProductListApi.jsx
+++ src/components/product/ProductListApi.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTable } from 'react-table'
 import { getProducts } from '../../api/productService'
 import {
@@ -9,7 +9,6 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Link,
 } from '@mui/material'
 
 const ProductList = () => {
@@ -29,72 +28,42 @@ const ProductList = () => {
     fetchProducts()
   }, [])
 
-  const columns = useMemo(() => {
-    if (products.length === 0) {
-      return []
-    }
-
-    // Nous allons définir une fonction pour chaque type de donnée que nous voulons afficher différemment
-    const renderCell = (key, value) => {
-      switch (key) {
-        case 'ficheTechnique':
-          return value ? (
-            <Link href={value} target="_blank">
-              PDF
-            </Link>
-          ) : (
-            'N/A'
-          )
-        case 'photos':
-          // Pour les photos, nous retournons des images au lieu de liens
-          return (
-            <div>
-              {value.map((photo, index) => (
-                <img
-                  key={index}
-                  src={`${serverBaseUrl}/${photo.replace(/\\/g, '/')}`} // Remplacez les backslashes par des slashes pour les URLs
-                  alt={`Product ${index}`} // Texte alternatif descriptif
-                  style={{ maxWidth: '100px', maxHeight: '100px' }} // Style exemple
-                />
-              ))}
-            </div>
-          )
-        case 'videos':
-          // Pour les vidéos, nous retournons des liens cliquables
-          return value.map((video, index) => (
-            <Link key={index} href={video} target="_blank">{`Video ${
-              index + 1
-            }`}</Link>
-          ))
-        case 'SKU':
-          // Pour SKU, qui est un tableau d'objets, nous affichons chaque propriété
-          return value.map((sku, index) => (
-            <div key={index}>
-              {Object.entries(sku).map(([field, val]) => (
-                <p key={field}>{`${field}: ${val}`}</p>
-              ))}
-            </div>
-          ))
-        default:
-          // Par défaut, nous retournons simplement la valeur
-          return value
-      }
-    }
-
-    // Maintenant nous mappons les clés des produits à des colonnes
-    return Object.keys(products[0]).map((key) => {
-      return {
-        Header: key.charAt(0).toUpperCase() + key.slice(1),
-        accessor: key,
-        Cell: ({ cell: { value } }) => renderCell(key, value),
-      }
-    })
-  }, [products, serverBaseUrl])
-
-  const tableInstance = useTable({ columns, data: products })
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Photo',
+        accessor: 'photos',
+        Cell: ({ value }) =>
+          value && value.length > 0 ? (
+            <img
+              src={`${serverBaseUrl}/${value[0].replace(/\\/g, '/')}`}
+              alt={`Product`}
+              style={{ maxWidth: '100px', maxHeight: '100px' }}
+            />
+          ) : null,
+      },
+      {
+        Header: 'Référence',
+        accessor: 'reference',
+      },
+      {
+        Header: 'Prix',
+        accessor: 'prixVente',
+      },
+      {
+        Header: 'Stock',
+        accessor: 'stock',
+      },
+      {
+        Header: 'Catégorie',
+        accessor: 'categorie',
+      },
+    ],
+    [serverBaseUrl],
+  )
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance
+    useTable({ columns, data: products })
 
   return (
     <TableContainer component={Paper}>
