c3c900536c2e49d408f13d08c7a1b9ea83d8b86e
chris <5sensprod@gmail.com>
2023-11-20 08:45:58 +0100

    defacto

--- server/server.js
+++ server/server.js
@@ -1,22 +1,46 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
-const productRoutes = require('./routes/productRoutes')
-
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = 5000
+const { getProducts } = require('../main/database/productDbOperations.js')
 
-app.use(bodyParser.json())
-app.use(cors())
-
-app.use('/api/products', productRoutes)
-
-app.get('/test', (req, res) => {
-  res.send('Le serveur fonctionne correctement.')
-})
-
-app.listen(PORT, () =>
-  console.log(`Serveur express écoutant sur le port ${PORT}`),
+// Configurez CORS pour autoriser les requêtes de votre domaine client
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Autoriser le domaine client à faire des requêtes
+  }),
 )
 
+// Route de test pour vérifier que le serveur fonctionne
+app.get('/test', (req, res) => {
+  res.send('Test response from Express')
+})
+
+// Route pour obtenir les produits
+app.get('/get-products', (req, res) => {
+  getProducts((err, products) => {
+    if (err) {
+      console.error('Error fetching products:', err)
+      res.status(500).json({
+        success: false,
+        message: 'Erreur lors de la récupération des produits',
+      })
+    } else {
+      res.json({ success: true, products })
+    }
+  })
+})
+
+if (require.main === module) {
+  app.listen(PORT, () =>
+    console.log(`Express server listening on port ${PORT}`),
+  )
+}
+
+// Exporter l'app pour pouvoir la démarrer à partir de electron.js en mode production
 module.exports = app
+
+// marche en dev
+// app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
+
+// module.exports = app
