ef84ef5b175026f241685e146ccf0c6a2cba1d5c
chris <5sensprod@gmail.com>
2023-11-28 09:12:01 +0100

    refacto Cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import theme from './theme/theme'
 import './i18n'
 import ProductSearch from './components/product/ProductSearch'
 import { CartProvider } from './contexts/CartContext'
+import { CompanyInfoProvider } from './contexts/CompanyInfoContext' // Importer le CompanyInfoProvider
 import Cart from './components/Cart/Cart'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <CartProvider>
-        <Box sx={{ marginTop: '16px' }}>
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={6}>
-              <ProductSearch />
+      <CompanyInfoProvider>
+        <CartProvider>
+          <Box sx={{ marginTop: '16px' }}>
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={6}>
+                <ProductSearch />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Cart />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <Cart />
-            </Grid>
-          </Grid>
-        </Box>
-      </CartProvider>
+          </Box>
+        </CartProvider>
+      </CompanyInfoProvider>
     </ThemeProvider>
   )
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef, useEffect } from 'react'
+import React, { useContext, useState } from 'react'
 import { CartContext } from '../../contexts/CartContext'
 import CartItem from './CartItem'
 import OrderSummary from '../OrderSummary/OrderSummary'
@@ -17,7 +17,8 @@ import {
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { addInvoice } from '../../api/invoiceService'
-import { getCompanyInfo } from '../../api/userService'
+import InvoicePrintComponent from '../InvoicePrintComponent'
+import usePrintInvoice from '../../hooks/usePrintInvoice'
 
 const Cart = () => {
   const {
@@ -27,19 +28,18 @@ const Cart = () => {
     updateQuantity,
     updatePrice,
     removeItem,
-    // checkout,
     holdInvoice,
     resumeInvoice,
     deleteInvoice,
   } = useContext(CartContext)
 
+  const { printRef, handlePrint } = usePrintInvoice()
+
   const [paymentType, setPaymentType] = useState('CB')
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [invoiceData, setInvoiceData] = useState(null)
-  const printRef = useRef()
 
-  const taxRate = 0.2 // 20% par exemple
-
+  const taxRate = 0.2
   const isCurrentCartOnHold = onHoldInvoices.some(
     (invoice) => JSON.stringify(invoice.items) === JSON.stringify(cartItems),
   )
@@ -52,8 +52,8 @@ const Cart = () => {
     return items.reduce((acc, item) => {
       const itemPrice =
         item.prixModifie !== undefined ? item.prixModifie : item.prixVente
-      const totalItemPrice = (itemPrice * item.quantity).toFixed(2) // Arrondir à deux décimales
-      return (parseFloat(acc) + parseFloat(totalItemPrice)).toFixed(2) // Assurez-vous d'arrondir après chaque addition
+      const totalItemPrice = (itemPrice * item.quantity).toFixed(2)
+      return (parseFloat(acc) + parseFloat(totalItemPrice)).toFixed(2)
     }, 0)
   }
 
@@ -126,111 +126,6 @@ const Cart = () => {
       console.error('An error occurred while adding the invoice:', error)
     }
   }
-
-  // Fonction pour gérer l'impression de la facture
-  const handlePrint = () => {
-    const printContent = printRef.current
-    const windowPrint = window.open(
-      '',
-      '',
-      'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0',
-    )
-    windowPrint.document.write(printContent.innerHTML)
-    windowPrint.document.close()
-    windowPrint.focus()
-    windowPrint.print()
-    windowPrint.close()
-  }
-
-  const InvoicePrint = React.forwardRef(({ invoiceData }, ref) => {
-    const [companyInfo, setCompanyInfo] = useState(null)
-
-    useEffect(() => {
-      const fetchCompanyInfo = async () => {
-        try {
-          const info = await getCompanyInfo()
-          setCompanyInfo(info)
-        } catch (error) {
-          console.error('Failed to fetch company info:', error)
-        }
-      }
-
-      fetchCompanyInfo()
-    }, [])
-    // Formatage de la date pour l'affichage
-    const formattedDate = new Date(invoiceData.date).toLocaleDateString(
-      'fr-FR',
-      {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-      },
-    )
-
-    return (
-      <div ref={ref}>
-        {/* Entête de la facture */}
-        {companyInfo && (
-          <>
-            <Typography variant="h5">{companyInfo.name}</Typography>
-            <Typography variant="body1">{companyInfo.address}</Typography>
-            <Typography variant="body1">{companyInfo.city}</Typography>
-            <Typography variant="body1">Tél: {companyInfo.phone}</Typography>
-            <Typography variant="body1">Email: {companyInfo.email}</Typography>
-            <Typography variant="body1">TVA: {companyInfo.taxId}</Typography>
-          </>
-        )}
-        <Typography variant="h6">
-          <Typography variant="body1">
-            Facture #{invoiceData.invoiceNumber}
-          </Typography>
-        </Typography>
-        <Typography variant="body2">Date: {formattedDate}</Typography>
-        <Typography variant="body2">
-          Type de paiement: {invoiceData.paymentType}
-        </Typography>
-
-        {/* Liste des articles */}
-        <div>
-          {invoiceData.items.map((item, index) => (
-            <div key={index}>
-              <Typography variant="body2">
-                Référence: {item.reference}
-              </Typography>
-              <Typography variant="body2">Quantité: {item.quantite}</Typography>
-              <Typography variant="body2">
-                Prix unitaire HT: {item.puHT} €
-              </Typography>
-              <Typography variant="body2">
-                Prix unitaire TTC: {item.puTTC} €
-              </Typography>
-              <Typography variant="body2">
-                Taux TVA: {item.tauxTVA} %
-              </Typography>
-              <Typography variant="body2">
-                Montant TVA: {item.montantTVA} €
-              </Typography>
-              {item.remiseMajorationLabel && (
-                <Typography variant="body2">
-                  {item.remiseMajorationLabel}: {item.remiseMajorationValue}
-                </Typography>
-              )}
-            </div>
-          ))}
-        </div>
-
-        {/* Total de la facture */}
-        <Typography variant="body2">
-          Total TTC: {invoiceData.totalTTC} €
-        </Typography>
-
-        {/* Pied de page */}
-        <Typography variant="caption">Merci pour votre achat.</Typography>
-      </div>
-    )
-  })
 
   return (
     <>
@@ -364,7 +259,7 @@ const Cart = () => {
             La facture a été enregistrée avec succès.
           </Typography>
           {invoiceData && (
-            <InvoicePrint ref={printRef} invoiceData={invoiceData} />
+            <InvoicePrintComponent ref={printRef} invoiceData={invoiceData} />
           )}
           <Button onClick={handlePrint} variant="contained">
             Imprimer la facture
